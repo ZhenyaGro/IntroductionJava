0b3e6e5badad1ac2ce7fa102bed5ec3da628efc3
ZhenyaGro <bon4ios@gmail.com>
2022-11-27 17:09:04 +0300

    Removed log from task 1

--- Seminar2/Seminar2.java
+++ Seminar2/Seminar2.java
@@ -38,7 +38,7 @@ public class Seminar2 {
      */
 
     String inputParametrs = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
-    StringBuilder query = new StringBuilder("SELECT * FROM students WHERE ");
+    StringBuilder query = new StringBuilder("SELECT *\nFROM students\nWHERE ");
 
     String replacementChars = "[\"{},]";
     String[] parsedString = inputParametrs.replaceAll(replacementChars, "").split(" ");
@@ -46,17 +46,15 @@ public class Seminar2 {
     String[] parametrs = new String[2];
     for (int i = 0; i < parsedString.length; i++) {
       parametrs = parsedString[i].split(":");
-      System.out.println(parametrs[0] + " " + parametrs[1]);
 
       if (!parametrs[1].equals("null")) {
         if (i != 0)
           query.append(" AND ");
         query.append(parametrs[0] + " = \"" + parametrs[1] + "\"");
       }
-      System.out.println(query + "\n");
     }
 
-    System.out.println(query);
+    System.out.println("Итоговый запрос:\n" + query);
 
   }
 

b0c00de750b9a1dcca94c70399709c204dcf501d
ZhenyaGro <bon4ios@gmail.com>
2022-12-03 18:47:58 +0300

    Seminar 4 is completed

--- Seminar4/Seminar4.java
+++ Seminar4/Seminar4.java
@@ -28,8 +28,8 @@ public class Seminar4 {
   public static void task1() {
     // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который
     // вернет “перевернутый” список.
-    int size = 10;
-    LinkedList<Integer> elements = createList(size);
+    int listSize = 10;
+    LinkedList<Integer> elements = createList(listSize);
     System.out.println("Исходный список: " + elements);
     LinkedList<Integer> revercedElements = reverceList(elements);
     System.out.println("Перевернутый список: " + revercedElements);
@@ -42,7 +42,40 @@ public class Seminar4 {
      * - dequeue() - возвращает первый элемент из очереди и удаляет его,
      * - first() - возвращает первый элемент из очереди, не удаляя.
      */
+    int queueSize = 10;
+    LinkedList<Integer> queue = createList(queueSize);
+    System.out.println("Начальная очередь: " + queue);
 
+    System.out.println(
+        "Выберите действие:\n1 - Добавить элемент\n2 - Удалить первый элемент\n3 - Показать первый элемент\n4 - Выход");
+
+    Scanner str = new Scanner(System.in);
+    String choosenAction = str.next();
+
+    switch (choosenAction) {
+      case "1":
+        enqueue(queue);
+        System.out.println("Очередь измена: " + queue);
+        break;
+
+      case "2":
+        int removedEl = dequeue(queue);
+        System.out.println("Удален первый элемент = " + removedEl + "\nТекущая очередь: " + queue);
+        break;
+
+      case "3":
+        int firstEl = first(queue);
+        System.out.println("Первый элемент очереди = " + firstEl);
+        break;
+
+      case "4":
+        break;
+
+      default:
+        System.out.println("Некорректный ввод");
+        break;
+    }
+    str.close();
   }
 
   public static LinkedList<Integer> createList(int size) {
@@ -54,7 +87,7 @@ public class Seminar4 {
     return list;
   }
 
-  public static LinkedList<Integer> reverceList(LinkedList list) {
+  public static LinkedList<Integer> reverceList(LinkedList<Integer> list) {
     LinkedList<Integer> revercedList = new LinkedList<>();
 
     int length = list.size();
@@ -63,5 +96,34 @@ public class Seminar4 {
     }
 
     return revercedList;
+  }
+
+  // enqueue() - помещает элемент в конец очереди
+  public static void enqueue(LinkedList<Integer> list) {
+    System.out.print("Добавление элемента в конец очереди.\nВведите элемент: ");
+    Scanner str = new Scanner(System.in);
+    try {
+      int element = str.nextInt();
+      list.add(element);
+    } catch (Exception err) {
+      System.out.println("Некорректный ввод.\nОшибка: " + err + "\nОчередь не была изменена");
+    }
+    str.close();
+  }
+
+  // dequeue() - возвращает первый элемент из очереди и удаляет его
+  public static int dequeue(LinkedList<Integer> list) {
+    if (list.size() > 0)
+      return list.removeFirst();
+    System.out.println("В очереди нет элементов");
+    return -1;
+  }
+
+  // first() - возвращает первый элемент из очереди, не удаляя
+  public static int first(LinkedList<Integer> list) {
+    if (list.size() > 0)
+      return list.getFirst();
+    System.out.println("В очереди нет элементов");
+    return -1;
   }
 }

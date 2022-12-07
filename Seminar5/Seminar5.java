0025461c92337f44a6efd665a1982df2d2231dff
ZhenyaGro <bon4ios@gmail.com>
2022-12-07 23:06:48 +0300

    Task 1 from seminar 5

---
+++ Seminar5/Seminar5.java
@@ -0,0 +1,70 @@
+package Seminar5;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Seminar5 {
+  public static void main(String[] args) {
+    System.out.print("Выбор задания (1 - 2): ");
+    Scanner str = new Scanner(System.in);
+    String choosenTask = str.next();
+
+    switch (choosenTask) {
+      case "1":
+        task1();
+        break;
+
+      case "2":
+        task2();
+        break;
+
+      default:
+        System.out.println("Некорректный ввод");
+        break;
+    }
+    str.close();
+  }
+
+  public static void task1() {
+    // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1
+    // человек может иметь несколько телефонов.
+    Map<String, String> phoneBook = new HashMap<>();
+    phoneBook.putIfAbsent("79213242685", "Иванов Иван Иванович");
+    phoneBook.putIfAbsent("79227557737", "Петров Петр Петрович");
+    phoneBook.putIfAbsent("79356335565", "Иванов Иван Иванович");
+    phoneBook.putIfAbsent("79335566358", "Васильев Василий Васильевич");
+
+    for (String number : phoneBook.keySet()) {
+      System.out.println(String.format("Телефон %s принадлежит контакту: %s", number, phoneBook.get(number)));
+    }
+  }
+
+  public static void task2() {
+    /*
+     * Пусть дан список сотрудников:
+     * 
+     * Иван Иванов
+     * Светлана Петрова
+     * Кристина Белова
+     * Анна Мусина
+     * Анна Крутова
+     * Иван Юрин
+     * Петр Лыков
+     * Павел Чернов
+     * Петр Чернышов
+     * Мария Федорова
+     * Марина Светлова
+     * Мария Савина
+     * Мария Рыкова
+     * Марина Лугова
+     * Анна Владимирова
+     * Иван Мечников
+     * Петр Петин
+     * Иван Ежов
+     * 
+     * Написать программу, которая найдет и выведет повторяющиеся имена с
+     * количеством повторений. Отсортировать по убыванию популярности.
+     */
+  }
+}

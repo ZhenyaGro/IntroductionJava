69f126f12a3ca7b785e4618b4d06bcd913ebea5f
ZhenyaGro <bon4ios@gmail.com>
2022-12-11 18:14:44 +0300

    Class to another file

--- Seminar6/Seminar6.java
+++ Seminar6/Seminar6.java
@@ -1,60 +1,16 @@
 package Seminar6;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Seminar6 {
-  public static class Laptop {
-    private String manufacturer, os, color;
-    private int ram;
-    private int hdDiscSpace;
-
-    public Laptop(String manufacturer, int ram, int hdDiscSpace, String os, String color) {
-      this.manufacturer = manufacturer;
-      this.ram = ram;
-      this.hdDiscSpace = hdDiscSpace;
-      this.os = os;
-      this.color = color;
-    }
-
-    public Object getProperty(String propNum) {
-      switch (propNum) {
-        case "1":
-          return this.manufacturer;
-
-        case "2":
-          return this.ram;
-
-        case "3":
-          return this.hdDiscSpace;
-
-        case "4":
-          return this.os;
-
-        case "5":
-          return this.color;
-
-        default:
-          return "Нет характеристики";
-      }
-    }
-
-    @Override
-    public String toString() {
-      return String.format(
-          "%s ноутбук %s на операционной системе %s с %dГб оперативной памяти и %dГб дискового пространства",
-          this.color,
-          this.manufacturer, this.os, this.ram, this.hdDiscSpace);
-    }
-  }
   // Текст задания в файле "task.txt"
-
   public static void main(String[] args) {
     try {
       File file = new File("./Seminar6/laptops.txt");
